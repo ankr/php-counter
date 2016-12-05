b9c74619d11602445d16f37139df6494cbb81e00
Andreas Kristiansen <mail@ankr.dk>
2016-12-05 22:01:58 +0100

    Doc blocks

--- tests/Counter/CounterTest.php
+++ tests/Counter/CounterTest.php
@@ -9,21 +9,36 @@ use PHPUnit_Framework_TestCase as TestCase;
 class CounterTest extends TestCase
 {
 
+    /**
+     * Setup
+     *
+     * @return void
+     */
     public function setUp()
     {
         parent::setUp();
         Counter::removeAll();
     }
 
+    /**
+     * Test exception for non-existing counter
+     *
+     * @return void
+     */
     public function testGetException()
     {
         $this->setExpectedException(CounterException::class, 'Counter "foo" not found!');
         Counter::get('foo');
     }
 
+    /**
+     * Testing get & set
+     *
+     * @return void
+     */
     public function testSetGet()
     {
-        Counter::set('foo', 3); // Before it exists
+        Counter::set('foo', 3);
         $this->assertEquals(3, Counter::get('foo'));
 
         Counter::inc('foo');
@@ -33,6 +48,11 @@ class CounterTest extends TestCase
         $this->assertEquals(3, Counter::get('foo'));
     }
 
+    /**
+     * Test increment
+     *
+     * @return void
+     */
     public function testIncrement()
     {
         Counter::inc('foo');
@@ -49,6 +69,11 @@ class CounterTest extends TestCase
         $this->assertEquals(Counter::get('bar'), 3);
     }
 
+    /**
+     * Test decrement
+     *
+     * @return void
+     */
     public function testDecrement()
     {
         Counter::inc('foo');
