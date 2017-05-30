eb1d4d526bc01d264af64eb04066be6cace7ca78
Andreas Kristiansen <mail@ankr.dk>
2017-05-30 16:18:05 +0200

    Declare strict types

--- src/Countable.php
+++ src/Countable.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ankr\Counter;
 
 use Countable as CountableInterface;
--- src/Counter.php
+++ src/Counter.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace ankr\Counter;
 
 use ankr\Counter\Countable;
